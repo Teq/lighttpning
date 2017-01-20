399f0e69c69ae65347032e1cad66978be11cccc1
Andrey Tselischev <vorteq@gmail.com>
2017-01-20 13:20:35 +0300

    Removed conditions from StringBuffer unit-tests

--- test/string_buffer.cpp
+++ test/string_buffer.cpp
@@ -7,34 +7,142 @@ using namespace Lighttpning;
 
 //SCENARIO("StringBuffer can be copy/move constructed/assigned and destructed properly") {}
 
-SCENARIO("StringBuffer can change capacity and size") {
+SCENARIO("StringBuffer can change its capacity and size") {
 
-    auto CAN_HANDLE_CAPACITY_AND_SIZE_CHANGES = [](StringBuffer& strbuff) {
+    GIVEN("empty buffer with default (zero) capacity") {
 
-        REQUIRE(strbuff.size() <= strbuff.capacity());
+        StringBuffer strbuff;
+
+        REQUIRE(strbuff.size() == 0);
+        REQUIRE(strbuff.capacity() == 0);
+
+        WHEN("the size is increased") {
+
+            size_t newSize = strbuff.size() + 1;
+
+            strbuff.resize(newSize);
+
+            THEN("the size increases, the capacity should be no less than size") {
+                REQUIRE(strbuff.size() == newSize);
+                REQUIRE(strbuff.capacity() >= newSize);
+            }
+        }
+
+        WHEN("the capacity is increased") {
+
+            size_t newCapacity = strbuff.capacity() + 1;
+            size_t size = strbuff.size();
+
+            strbuff.reserve(newCapacity);
+
+            THEN("the capacity increases, the size stays the same") {
+                REQUIRE(strbuff.size() == size);
+                REQUIRE(strbuff.capacity() == newCapacity);
+            }
+        }
+
+    }
+
+    GIVEN("empty buffer with capacity of N items") {
+
+        const size_t N = 12;
+        StringBuffer strbuff(N);
+
+        REQUIRE(strbuff.size() == 0);
+        REQUIRE(strbuff.capacity() >= N);
 
         WHEN("the size is increased") {
 
             AND_WHEN("new size <= capacity") {
 
                 size_t capacity = strbuff.capacity();
-                size_t newSize = strbuff.size() + 1;
+                size_t newSize = N - 1;
 
-                if (newSize <= capacity) {
+                strbuff.resize(newSize);
 
-                    strbuff.resize(newSize);
-
-                    THEN("the size increases, the capacity stays the same") {
-                        REQUIRE(strbuff.size() == newSize);
-                        REQUIRE(strbuff.capacity() == capacity);
-                    }
-
-                } // else not applicable
+                THEN("the size increases, the capacity stays the same") {
+                    REQUIRE(strbuff.size() == newSize);
+                    REQUIRE(strbuff.capacity() == capacity);
+                }
             }
 
             AND_WHEN("new size > capacity") {
 
-                size_t newSize = strbuff.capacity() + 1;
+                size_t newSize = N + 1;
+
+                strbuff.resize(newSize);
+
+                THEN("the size increases, the capacity should be no less than size") {
+                    REQUIRE(strbuff.size() == newSize);
+                    REQUIRE(strbuff.capacity() >= newSize);
+                }
+
+            }
+
+        }
+
+        WHEN("the capacity is increased") {
+
+            size_t newCapacity = N + 1;
+            size_t size = strbuff.size();
+
+            strbuff.reserve(newCapacity);
+
+            THEN("the capacity increases, the size stays the same") {
+                REQUIRE(strbuff.size() == size);
+                REQUIRE(strbuff.capacity() == newCapacity);
+            }
+        }
+
+        WHEN("the capacity is reduced") {
+
+            size_t newCapacity = N - 1;
+            size_t size = strbuff.size();
+
+            strbuff.reserve(newCapacity);
+
+            THEN("the capacity decreases, the size stays the same") {
+                REQUIRE(strbuff.size() == size);
+                REQUIRE(strbuff.capacity() == newCapacity);
+            }
+
+        }
+
+    }
+
+    GIVEN("non-empty buffer with capacity of N items") {
+
+        const size_t N = 32;
+        StringBuffer strbuff(N);
+
+        REQUIRE(strbuff.capacity() == N);
+
+        char cStr[] = "Hello world";
+        size_t cStrSize = strlen(cStr);
+        strbuff += cStr;
+
+        REQUIRE(strbuff.size() == cStrSize);
+        REQUIRE(strbuff.capacity() >= cStrSize);
+
+        WHEN("the size is increased") {
+
+            size_t capacity = strbuff.capacity();
+
+            AND_WHEN("new size <= capacity") {
+
+                size_t newSize = capacity - 1;
+
+                strbuff.resize(newSize);
+
+                THEN("the size increases, the capacity stays the same") {
+                    REQUIRE(strbuff.size() == newSize);
+                    REQUIRE(strbuff.capacity() == capacity);
+                }
+            }
+
+            AND_WHEN("new size > capacity") {
+
+                size_t newSize = capacity + 1;
 
                 strbuff.resize(newSize);
 
@@ -50,19 +158,14 @@ SCENARIO("StringBuffer can change capacity and size") {
         WHEN("the size is reduced") {
 
             size_t capacity = strbuff.capacity();
-            size_t size = strbuff.size();
+            size_t newSize = strbuff.size() - 1;
 
-            if (size > 0) {
+            strbuff.resize(newSize);
 
-                size_t newSize = size - 1;
-                strbuff.resize(newSize);
-
-                THEN("the size reduces, the capacity stays the same") {
-                    REQUIRE(strbuff.size() == newSize);
-                    REQUIRE(strbuff.capacity() == capacity);
-                }
-
-            } // else not applicable
+            THEN("the size reduces, the capacity stays the same") {
+                REQUIRE(strbuff.size() == newSize);
+                REQUIRE(strbuff.capacity() == capacity);
+            }
 
         }
 
@@ -81,84 +184,35 @@ SCENARIO("StringBuffer can change capacity and size") {
 
         WHEN("the capacity is reduced") {
 
-            size_t capacity = strbuff.capacity();
             size_t size = strbuff.size();
 
-            if (capacity > 0) {
+            AND_WHEN("new capacity >= size") {
 
-                size_t newCapacity = capacity - 1;
+                size_t newCapacity = size + 1;
 
-                if (newCapacity >= size) {
+                strbuff.reserve(newCapacity);
 
-                    AND_WHEN("new capacity >= size") {
+                THEN("the capacity decreases, the size stays the same") {
+                    REQUIRE(strbuff.size() == size);
+                    REQUIRE(strbuff.capacity() == newCapacity);
+                }
+            }
 
-                        strbuff.reserve(newCapacity);
 
-                        THEN("the capacity decreases, the size stays the same") {
-                            REQUIRE(strbuff.size() == size);
-                            REQUIRE(strbuff.capacity() == newCapacity);
-                        }
-                    }
+            AND_WHEN("new capacity < size") {
 
-                } // else not applicable
+                size_t newCapacity = size - 1;
 
-                if (size > 0) {
+                strbuff.reserve(newCapacity);
 
-                    newCapacity = size - 1;
-
-                    AND_WHEN("new capacity < size") {
-
-                        strbuff.reserve(newCapacity);
-
-                        THEN("the size and capacity both decreases") {
-                            REQUIRE(strbuff.size() == newCapacity);
-                            REQUIRE(strbuff.capacity() == newCapacity);
-                        }
-                    }
-
-                } // else not applicable
-
-            } // else not applicable
+                THEN("the size and capacity both decreases") {
+                    REQUIRE(strbuff.size() == newCapacity);
+                    REQUIRE(strbuff.capacity() == newCapacity);
+                }
+            }
 
         }
 
-    };
-
-    GIVEN("empty buffer with default (zero) capacity") {
-
-        StringBuffer strbuff;
-
-        REQUIRE(strbuff.size() == 0);
-        REQUIRE(strbuff.capacity() == 0);
-        REQUIRE(strbuff.buff() == nullptr);
-
-        CAN_HANDLE_CAPACITY_AND_SIZE_CHANGES(strbuff);
-    }
-
-    GIVEN("empty buffer with capacity of N items") {
-
-        const size_t N = 12;
-        StringBuffer strbuff(N);
-
-        REQUIRE(strbuff.size() == 0);
-        REQUIRE(strbuff.capacity() >= N);
-        REQUIRE(strbuff.buff() != nullptr);
-
-        CAN_HANDLE_CAPACITY_AND_SIZE_CHANGES(strbuff);
-    }
-
-    GIVEN("non-empty buffer") {
-
-        StringBuffer strbuff;
-        char cStr[] = "Hello world";
-        size_t cStrLen = strlen(cStr);
-        strbuff += cStr;
-
-        REQUIRE(strbuff.size() == cStrLen);
-        REQUIRE(strbuff.capacity() >= cStrLen);
-        REQUIRE(strbuff.buff() != nullptr);
-
-        CAN_HANDLE_CAPACITY_AND_SIZE_CHANGES(strbuff);
     }
 
 }
